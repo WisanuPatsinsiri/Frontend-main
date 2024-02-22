80b92ecd4b4950f0aa6f9aed6b884fc340adbada
Wisanu <bestzazazazath@Gmail.com>
2024-02-22 15:37:17 +0700

    s

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var bodyParser = require('body-parser')
 const path = require("path")
 
 
-
-//const base_url = "http://node57329-nodepem.proen.app.ruk-com.cloud"
 const base_url = "http://node58983-wisanu-comebackk.proen.app.ruk-com.cloud"
 //const base_url = "http://localhost:3000"
 app.set("views", path.join(__dirname, "/public/views"))
