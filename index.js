e308c8e3857beec1af1f8191501476157214734f
WisanuPatsinsiri <bestzazazazath@gmail.com>
2024-02-22 19:42:01 +0700

    s

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var bodyParser = require('body-parser')
 const path = require("path")
 
 
-
-//const base_url = "http://node57329-nodepem.proen.app.ruk-com.cloud"
 //const base_url = "http://node59037-wisanu-come.proen.app.ruk-com.cloud:11720"
 const base_url = "http://localhost:3000"
 app.set("views", path.join(__dirname, "/public/views"))
