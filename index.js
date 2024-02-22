f61598b6e9e205a3c8f5de622d5063cc265fed57
WisanuPatsinsiri <bestzazazazath@gmail.com>
2024-02-22 22:15:02 +0700

    s

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var bodyParser = require('body-parser')
 const path = require("path")
 
 
-const base_url = "http://node59040-bestt.proen.app.ruk-com.cloud"
-//const base_url = "http://localhost:3000"
+//const base_url = "http://node59040-bestt.proen.app.ruk-com.cloud"
+const base_url = "http://localhost:3000"
 
 app.set("views", path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs')
